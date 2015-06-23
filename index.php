431bada35c0e6b06ecbf1dcc40489da0d834440a
Caleb Nelson <calebnelson@mac.com>
2015-06-23 11:53:22 -0700

    added coming soon and gif to index

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
+<!DOCTYPE html>
 <html>
+<head>
+	<title>Demo Title</title>
+</head>
 <body>
-<h4>Stick around for a while</h4>
+	<h1>Ye Olde Tech Repair</h1>
+	<h4>Coming Soon...</h4>
+	<img src="https://i.imgur.com/l5oUYu4.jpg">
 </body>
 </html>
